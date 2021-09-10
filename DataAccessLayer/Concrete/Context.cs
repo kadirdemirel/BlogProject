d31b4ea22843472cda9c0cb254f3eaa216267936
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-09-10 15:38:14 +0300

    Context sınıfı oluşturuldu,

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+       protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-RLC2A77;database=CoreBlogDb; integrated security=true");
+        }
+    }
+}
