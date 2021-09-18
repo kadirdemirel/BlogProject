cca2ccf06e0aa671576d33175b5607423001af52
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-09-18 11:05:24 +0300

    Business katmanında bulunan service ve managerlar oluşturuldu.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
