b54b6fd723f637d73111cf1e761ad5a0ce8c5e89
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-10-12 14:20:49 +0300

    FluentValidation kütüphaneleri kuruldu. Writer sınıfı için Validasyonlar yazıldı. Register sayfasına validasyonlar entegre edildi.

---
+++ BusinessLayer/ValidationRules/FluentValidation/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Boş Geçilemez");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Boş Geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Boş Geçilemez");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("İsim en az 2 karakter olmalıdır.");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("İsim en fazla 50 karakter olmalıdır.");
+
+        }
+    }
+}
--- PresentationLayer/Controllers/RegisterController.cs
+++ PresentationLayer/Controllers/RegisterController.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Abstract;
+using BusinessLayer.ValidationRules.FluentValidation;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -23,10 +25,26 @@ namespace PresentationLayer.Controllers
         [HttpPost]
         public IActionResult Index(Writer writer)
         {
-            writer.WriterStatus = true;
-            writer.WriterAbout = "Deneme";
-            _writerService.Add(writer);
-            return RedirectToAction("Index", "Blog");
+            WriterValidator validationRules = new WriterValidator();
+            ValidationResult validationResult = validationRules.Validate(writer);
+
+            if (validationResult.IsValid)
+            {
+                writer.WriterStatus = true;
+                writer.WriterAbout = "Deneme";
+                _writerService.Add(writer);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+
         }
     }
 }
