749fa5007d3f75fcb0fe501d774b91d585de4095
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-10-15 16:10:13 +0300

    Vitrin tarafında iyileştirmeler yapıldı.

---
+++ PresentationLayer/Controllers/WriterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Controllers
+{
+    public class WriterController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
