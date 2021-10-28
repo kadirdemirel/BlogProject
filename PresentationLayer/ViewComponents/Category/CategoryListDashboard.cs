0ba5368c7b73e752fae43e39683fcf6e664b9203
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-10-28 14:50:30 +0300

    Dashboard sayfasında "Kategoriler" kısmı ViewComponent yapısına alındı.

---
+++ PresentationLayer/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        ICategoryService _categoryService;
+        public CategoryListDashboard(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var getAll = _categoryService.GetAll();
+            return View(getAll);
+        }
+    }
+}
