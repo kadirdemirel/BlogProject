d327a0596ab1449b1dd1a1d9cd96f5dab7845ddb
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-10-28 14:25:23 +0300

    Dashboard sayfasında düzenleme yapıldı. Dashboard sayfasında "Son Bloglar" kısmı ViewComponent yapısına alındı.

---
+++ PresentationLayer/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.ViewComponents.Blog
+{
+    public class BlogListDashboard:ViewComponent
+    {
+        IBlogService _blogService;
+        public BlogListDashboard(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var selectedItems = _blogService.GetListWithCategory();
+            return View(selectedItems);
+        }
+    }
+}
