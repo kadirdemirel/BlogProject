ecdad7b73d0bf16ba1ae4347b32c538520fa4f5c
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-11-06 12:10:58 +0300

    Yazar paneli dashboard istatistikleri oluşturuldu.

--- PresentationLayer/Controllers/DashboardController.cs
+++ PresentationLayer/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,9 +11,20 @@ namespace PresentationLayer.Controllers
 {
     public class DashboardController : Controller
     {
+        IBlogService _blogService;
+       
+        public DashboardController(IBlogService blogService)
+        {
+            _blogService = blogService;
+      
+        }
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context context = new Context();
+            ViewBag.toplamBlogSayisi = context.Blogs.Count().ToString();
+            ViewBag.yazarBlogSayisi = context.Blogs.Where(x => x.WriterID == 1).Count();
+            ViewBag.toplamKategoriSayisi = context.Categories.Count();
             return View();
         }
     }
