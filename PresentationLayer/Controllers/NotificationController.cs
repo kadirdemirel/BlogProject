6d15b10a40fe7267cd40c8fb45e1967a0bf5ad31
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-11-16 21:58:25 +0300

    Tüm bildirimler listelendi.

--- PresentationLayer/Controllers/NotificationController.cs
+++ PresentationLayer/Controllers/NotificationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,20 @@ namespace PresentationLayer.Controllers
 {
     public class NotificationController : Controller
     {
+        INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var getAll = _notificationService.GetAll();
+            return View(getAll);
         }
     }
 }
