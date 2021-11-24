d75905dafc26b51cef0bd8790bc521d0c282f56c
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2021-11-24 11:19:11 +0300

    Area yapısı oluşturuldu.

--- PresentationLayer/Startup.cs
+++ PresentationLayer/Startup.cs
@@ -124,6 +124,10 @@ namespace PresentationLayer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                name: "areas",
+                 pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
